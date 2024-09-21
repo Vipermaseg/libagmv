fd58b1a1179f198566fbbc65d8457e2c629cf0c7
Víctor Pérez <vipermaseg@gmail.com>
2024-09-21 16:44:43 +0200

    Futher work into making this a toolchain that works for me.

--- examples/simple_gba_encoding/hitme.sh
+++ examples/simple_gba_encoding/hitme.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+# typedef enum AGMV_IMG_TYPE{
+# 	AGMV_IMG_BMP = 0x1,
+# 	AGMV_IMG_TGA = 0x2,
+# 	AGMV_IMG_TIM = 0x3,
+# 	AGMV_IMG_PCX = 0x4,
+# 	AGMV_IMG_LMP = 0x5,
+# 	AGMV_IMG_PVR = 0x6,
+# 	AGMV_IMG_GXT = 0x7,
+# 	AGMV_IMG_BTI = 0x8,
+# 	AGMV_IMG_3DF = 0x9,
+# 	AGMV_IMG_PPM = 0x0A,
+# 	AGMV_IMG_LBM = 0x0B,
+# }AGMV_IMG_TYPE;
+
+# typedef enum AGMV_OPT{
+# 	AGMV_OPT_I       = 0x1,  /* 512 COLORS, BITSTREAM V1, HEAVY PDIFS */
+# 	AGMV_OPT_II      = 0x2,  /* 256 COLORS, BITSTREAM V2, LIGHT PDIFS */
+# 	AGMV_OPT_III     = 0x3,  /* 512 COLORS, BITSTREAM V1, LIGHT PDIFS */
+# 	AGMV_OPT_ANIM    = 0x4,  /* 256 COLORS, BITSTREAM V2, HEAVY PDIFS */
+# 	AGMV_OPT_GBA_I   = 0x5,  /* 512 COLORS, BITSTREAM V1, HEAVY PDIFS, REDUCED RES */
+# 	AGMV_OPT_GBA_II  = 0x6,  /* 256 COLORS, BITSTREAM V2, HEAVY PDIFS, REDUCED RES */
+# 	AGMV_OPT_GBA_III = 0x7,  /* 512 COLORS, BITSTREAM V2, LIGHT PDIFS, REDUCED RES */
+# 	AGMV_OPT_NDS     = 0x8,  /* 512 COLORS, BITSTREAM V1, HEAVY PDIFS, REDUCED RES*/
+# }AGMV_OPT;
+
+# typedef enum AGMV_QUALITY{
+# 	AGMV_HIGH_QUALITY = 0x1,
+# 	AGMV_MID_QUALITY  = 0x2,
+# 	AGMV_LOW_QUALITY  = 0x3,
+# }AGMV_QUALITY;
+
+# typedef enum AGMV_COMPRESSION{
+# 	AGMV_LZSS_COMPRESSION = 0x1,
+# 	AGMV_LZ77_COMPRESSION = 0x2,
+# }AGMV_COMPRESSION;
+
 # Loop through all mp4 files
 cd raw_videos
 for file in *.mp4; do
@@ -10,5 +46,15 @@ for file in *.mp4; do
   mkdir -p ../output/"$filename"
   
   # Run ffmpeg to process the video
-  ffmpeg -i "$file" -filter:v "fps=8,scale=-1:160,crop=240:160" -map 0:v -c:v bmp -pix_fmt pal8 ../output/"$filename"/frame_%d.bmp -map 0:a ../output/"$filename.wav"
+  ffmpeg -y -i "$file" \
+    -filter:v "fps=8,scale=-1:160,crop=240:160" \
+    -map 0:v -c:v bmp -pix_fmt pal8 ../output/"$filename"/frame_%d.bmp \
+    -map 0:a -ac 1 -ar 16000 -acodec pcm_s8 -f s8 ../output/"$filename.raw"
+  ../main 1724 240 160 8 ../output/"$filename.raw" 16000 ../output/"$filename.agmv" ../output/"$filename" frame_ 1 6 3 1
+  mv -f GBA_GEN_AGMV.h ../output/"$filename.h"
 done
+cp ../output/"$filename.h" ../../simple_gba_video/include/GBA_GEN_AGMV.h
+cd ../../simple_gba_video
+make clean
+make
+mv simple_gba_video.gba ../simple_gba_encoding/output/"$filename.gba"
--- examples/simple_gba_encoding/simple_video.c
+++ examples/simple_gba_encoding/simple_video.c
@@ -1,18 +1,5 @@
 /********************************************
-*   Adaptive Graphics Motion Video
-*
-*   Copyright (c) 2024 Ryandracus Chapman
-*
-*   Library: libagmv
-*   File: simple_video.c
-*   Date: 6/7/2024
-*   Version: 1.1
-*   Updated: 6/13/2024
-*   Author: Ryandracus Chapman
-*
-********************************************/
-#include <agmv.h>
-
+ * baseline
 int main(){
 	u32 num_of_frames = 1724;
 	u32 width = 240;
@@ -24,4 +11,36 @@ int main(){
 	AGMV_EncodeAGMV(agmv,"videoplaybackniAudioANIM.agmv","output/videoplayback","frame_",AGMV_IMG_BMP,1,num_of_frames,width,height,fps,AGMV_OPT_GBA_II,AGMV_LOW_QUALITY,AGMV_LZSS_COMPRESSION);
 	
 	return 0;
+}
+********************************************/
+#include <agmv.h>
+#include <stdio.h>      /* printf, fgets */
+#include <stdlib.h>     /* atoi */
+
+int main(int argc, char *argv[]) {
+    // Check if all required arguments are provided
+    if (argc != 14) {
+        fprintf(stderr, "Usage: %s <num_of_frames> <width> <height> <fps> <input_audio_file> <audio_sample_rate> <output_agmv_file>  <output_frame_directory> <output_frame_prefix> <img_format> <gba_option> <quality_option> <compression_option>\n", argv[0]);
+        return 1;
+    }
+
+    u32 num_of_frames = atoi(argv[1]);
+    u32 width = atoi(argv[2]);
+    u32 height = atoi(argv[3]);
+    u32 fps = atoi(argv[4]);
+    char *input_audio_file = argv[5];
+    int audio_sample_rate = atoi(argv[6]);
+    char *output_agmv_file = argv[7];
+    char *output_frame_directory = argv[8];
+    char *output_frame_prefix = argv[9];
+    int img_format = atoi(argv[10]);
+    int gba_option = atoi(argv[11]);
+    int quality_option = atoi(argv[12]);
+    int compression_option = atoi(argv[13]);
+
+    AGMV* agmv = CreateAGMV(num_of_frames - 1, width, height, fps);
+    AGMV_RawSignedPCMToAudioTrack(input_audio_file, agmv, 1, audio_sample_rate);
+    AGMV_EncodeAGMV(agmv, output_agmv_file, output_frame_directory, output_frame_prefix, img_format, 1, num_of_frames, width, height, fps, gba_option, quality_option, compression_option);
+
+    return 0;
 }
