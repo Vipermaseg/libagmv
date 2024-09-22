d29617443cdf4734c893c87a6c2721ff1d1eb755
Víctor Pérez <vipermaseg@gmail.com>
2024-09-22 09:41:50 +0200

    Removed things that didn't seem to do anything.

--- examples/simple_gba_video/source/main.c
+++ examples/simple_gba_video/source/main.c
@@ -3,14 +3,13 @@
 
 #define VRAM_F  0x6000000 
 #define VRAM_B	0x600A000
-#define RATE 1
 
 int IWRAM_DATA main(){
 	
 	SetVideoMode(AGMV_MODE_3);
 	EnableTimers();
 	
-	int lastFr=-1,FPS=0;  
+	int lastFr=-1; 
 	
 	REG_BG2PA = 128;
 	REG_BG2PD = 128;
@@ -19,17 +18,11 @@ int IWRAM_DATA main(){
 	
 	File* file = (File*)malloc(sizeof(File));
 	
-	Open(file,GBA_AGMV_FILE,4790547);
+	Open(file,GBA_AGMV_FILE,9270062);
 	
 	AGMV* agmv = AGMV_AllocResources(file);
 	
 	//AGMV_DisableAllAudio(agmv);
-	
-	*interrupt_enable = 0;
-    *interrupt_selection |= INTERRUPT_VBLANK;
-    *display_interrupts |= 0x08;
-    *interrupt_enable = 1;
-
     /* clear the sound control initially */
     *sound_control = 0;
 	
@@ -40,7 +33,7 @@ int IWRAM_DATA main(){
 		
 		scanKeys();
 		
-		if((REG_TM3CNT/RATE)!=lastFr){
+		if((REG_TM3CNT)!=lastFr){
 
 			if(button_pressed(BUTTON_RIGHT)){
 				AGMV_SkipForwards(file,agmv,10);
@@ -70,8 +63,7 @@ int IWRAM_DATA main(){
 			   AGMV_ResetVideo(file,agmv);
 		   }
 		   
-		   FPS+=1; if(lastFr>(REG_TM3CNT/RATE)){ FPS=0;}
-		   lastFr=(REG_TM3CNT/RATE);  
+		   lastFr=(REG_TM3CNT);  
 
 			VBlankIntrWait();		   
 		}		
